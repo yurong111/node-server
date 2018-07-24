03eebf2bdb5b7275490dd97a0dd7156267a21959
hwr <1092021277@qq.com>
2018-07-24 18:34:20 +0800

    node服务以及处理cors跨域

---
+++ api.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const cookie=require('cookie-parser'); 
+const app = express()
+
+/**
+ * 跨域处理方式
+ * 1. webpack-dev-server                浏览器携带cookie，不需要配置credentials
+ * 2. http-proxy-middleware + express   浏览器携带cookie，不需要配置credentials
+ * 3. nginx                             浏览器携带cookie，不需要配置credentials
+ * 4. 后台cors                           浏览器携带cookie，需要配置credentials
+ */
+
+// 后台处理跨域
+app.use(cors({
+  origin: true, // 设置了credentials: true，origin就不能设置为true
+  credentials: true, // 如果需要浏览器携带cookie信息时，需要设置为true,
+  methods: ['GET', 'PUT', 'POST'],
+}))
+
+// 设置cookie
+app.use(cookie());
+
+app.post('/api/post', (req, res) => res.send('post Hello World!'))
+
+app.get('/api/login', (req, res) => {
+  if(req.cookies.user){
+      res.send('欢迎在次登录')
+  }
+  else{
+      res.cookie('user',1,{ expires: new Date(Date.now() + 5000), httpOnly: true });
+      res.send('欢迎新用户登录')
+  }
+})
+
+
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
